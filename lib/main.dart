ea77e8815a21df895a0294b6159e59bfa5dbc57b
Muhammad Abdullah Waheed <mabdullahwaheed127@gmail.com>
2024-05-01 21:12:11 +0500

    Gender plus Age page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:db_final_project_fitness_app/screens/StartupScreen/StartupScreeb.dart';
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(const FitnessApp());
 }
 
-class FitnessApp extends StatelessWidget
-{
+class FitnessApp extends StatelessWidget {
   const FitnessApp({super.key});
   @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp
-    (color: Colors.black,
-    debugShowCheckedModeBanner: false,
-    home: StartupScreen(),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.black,
+      debugShowCheckedModeBanner: false,
+      home: StartupScreen(),
     );
   }
 }
