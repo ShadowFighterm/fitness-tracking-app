c1fa29c3cfe2306b2a9e153e1b09c331a14c28cb
Muhammad Abdullah Waheed <mabdullahwaheed127@gmail.com>
2024-06-10 07:12:20 +0500

    friends pages setting

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -58,10 +58,10 @@ class FitnessApp extends StatelessWidget {
         '/AddActivity': (context) => AddActivityPage(),
         '/workoutinput': (context) => const ExerciseInputPage(),
         '/searchPage': (context) => SearchPage(),
-        'todayworkoutplan': (context) => WorkoutDetailsPage(),
+        '/todayworkoutplan': (context) => WorkoutDetailsPage(),
       },
       debugShowCheckedModeBanner: false,
-      home: HomepageNavbar(),
+      home: StartupScreen(),
     );
   }
 }
